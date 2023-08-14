cad4bf7000b022a565978fbe51d233132f52ceb1
Lucas Bueno Cesario <lbc92@hotmail.com>
2023-08-13 22:06:34 -0300

    First commit

---
+++ src/main/java/br/ufsc/ine/polyglotimportcsv/PolyglotimportcsvApplication.java
@@ -0,0 +1,13 @@
+package br.ufsc.ine.polyglotimportcsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyglotimportcsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyglotimportcsvApplication.class, args);
+	}
+
+}
